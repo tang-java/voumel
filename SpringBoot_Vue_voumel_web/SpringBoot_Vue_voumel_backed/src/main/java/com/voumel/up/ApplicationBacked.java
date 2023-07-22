935566969f8c2051af49ed2328b98b5cf358ec1b
唐瑜翔 <happy_tang_yx@163.com>
2023-07-22 17:29:37 +0800

    创建仓库

---
+++ SpringBoot_Vue_voumel_web/SpringBoot_Vue_voumel_backed/src/main/java/com/voumel/up/ApplicationBacked.java
@@ -0,0 +1,12 @@
+package com.voumel.up;
+
+/**
+ * @author 小唐
+ * @version 1.0
+ * @project SpringBoot_Vue_voumel_parent
+ * @description PC后台的启动类
+ * @date 2023/7/22 17:18:04
+ */
+
+public class ApplicationBacked {
+}
